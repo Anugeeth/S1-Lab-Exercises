a674843419140292e2b41ca11986a4baf7e74a25
anugeeth <anugeeth2@gmail.com>
2020-02-13 20:50:15 +0530

    .

--- natno.c
+++ natno.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 void main(){
-int a,sum=0,i=1;
+int a,i=1;
 printf("Enter a number\t");
 scanf("%d",&a);
 while(i<=a){
- sum=sum+i;
+printf("%d \t",i);
  i++;
 }
-printf("sum = %d",sum);
 }
