6b17b6463e432262ae6f9bd74b27e5fd2ad3d546
anugeeth <anugeeth2@gmail.com>
2020-02-13 20:28:09 +0530

    Added natural nos.c

--- natno.c
+++ natno.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 void main(){
-int a,i=1;
+int a,sum=0,i=1;
 printf("Enter a number\t");
 scanf("%d",&a);
-while(i<a){
+while(i<=a){
  sum=sum+i;
  i++;
 }
-print("sum = %d",sum);
+printf("sum = %d",sum);
 }
--- vowels.c
+++ vowels.c
@@ -4,15 +4,15 @@ char a;
 printf("enter a charecter\t");
 scanf("%s",&a);
 switch(a){
-case 'a': printf("Vowel");
+case 'a': printf("Vowel\n");
 	break;
-case 'e': printf("Vowel");
+case 'e': printf("Vowel\n");
 	break;
-case 'i': printf("Vowel");
+case 'i': printf("Vowel\n");
 	break;
-case 'o': printf("Vowel");
+case 'o': printf("Vowel\n");
 	break;
-case 'u': printf("Vowel");
+case 'u': printf("Vowel\n");
 	break;
 default: printf("Not a vowel \n");
 }
